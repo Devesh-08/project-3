bbdbab5da3938c5981a0384818c663eeb5a77da4
Devesh <prajapatidevesh765@gmail.com>
2026-04-30 17:42:40 +0530

    update for test deployment

--- QRbackend/src/app.js
+++ QRbackend/src/app.js
@@ -5,7 +5,9 @@ const cookieParser = require("cookie-parser")
 const app=express()
 
 //middlewares
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
